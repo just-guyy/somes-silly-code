f91914e5f496727e7f74310def8b9ec66f2543a4
just-guyy <150066816+just-guyy@users.noreply.github.com>
2025-04-26 08:56:20 +0300

    Add files via upload

---
+++ MyToolBox.py
@@ -0,0 +1,89 @@
+import math
+variables = {}
+
+def factorial(x):
+    if x < 0:
+        raise ValueError(f"yo wtf {x} is a negative number lol")
+    elif x == int(x):
+        return math.factorial(int(x))
+    else:
+        return math.gamma(x + 1)
+
+
+def sin(input, accuracy=5):
+    output = 0
+    sign = 1
+    for i in range(1, accuracy*2-1, 2):
+        if sign==1:
+            output += input**i / factorial(i)
+            sign = 0
+        elif sign==0: 
+           output -= input**i / factorial(i)
+           sign = 1
+        else:
+            raise IndexError("wtf")
+    return output
+
+
+def cos(input, accuracy=5):
+    output = 0
+    sign = 1
+    for i in range(0, accuracy*2, 2):
+        if sign==1:
+            output += input**i / factorial(i)
+            sign = 0
+        elif sign==0: 
+           output -= input**i / factorial(i)
+           sign = 1
+        else:
+            raise IndexError("wtf")
+    return output
+
+
+def tan(input, accuracy=5):
+    return(sin(input, accuracy)/cos(input, accuracy))
+
+def radians(input):
+    return input/(3.14 / 180.0)
+
+def initialize_nn(id):
+    variables[id]
+    for i in range(1, variables[id]["layers"]):
+        v = "weights" + str(i) + str(i+1)
+        v2 = "layer" + str(i)
+        v3 = "layer" + str(i+1)
+        variables[id][v] = [0] * (variables[v2] * variables[id][v3])
+    for i in range(1, variables[id]["layers"]+1):
+        v = str(i)
+        vv = "layer" + v
+        w = "biases" + v
+        variables[id][w] = [0] * variables[id][vv]
+    for i in range(1, variables[id]["layers"]+1):
+        variables[id]["layer"+str(i)+"n"] = [0] * variables[id]["layer"+str(i)]
+
+def activation(input):
+    #relu
+    for i in range(len(list(input))):
+        if input[i] <= 0:
+            input[i] = 0
+    return input
+
+def nn_run(id):
+    stuff = variables[id]
+    stuff["temp"] = []
+    for i in range(0, stuff["layers"]-1):
+        del stuff["temp"]
+        stuff["temp"] = []
+        for l in range(0, stuff["layer"+str(i+2)]):
+            stuff["temp"].append(0)
+        v = str(i+1) + str(i+2)
+        w = "weights" + v
+        for j in range(0, len(stuff[w])):
+            x = len(stuff[w])
+            y = len(stuff["layer"+str(i+2)+"n"])
+            z = stuff[w][j]
+            jz = j%y
+            jy = int((j-jz)/y)
+            stuff["temp"][jz] = stuff["layer"+str(i+1)+"n"][jy] * z
+        stuff["layer"+str(i+2)+"n"] = activation([x - y for x, y in zip(stuff["temp"], stuff["biases"+str(i+2)])])
+    del stuff["temp"]
---
+++ main.py
@@ -0,0 +1,53 @@
+import MyToolBox as tb
+
+def delusional_attention(V, Q, K):
+    output = [[None]*len(V[_]) for _ in range(len(V))]
+    tokens = len(V)
+    for i in range(tokens):
+        wants = Q[i]
+        for j in range(tokens):
+            #if j == i:
+                #continue #no need in skipping the current token ig
+            temp = [K[j][_] * wants[_] for _ in range(len(K[j]))]
+            temp2 = sum(temp)
+            output[i] = [V[j][_] * (temp[j]/temp2) for _ in range(len(temp))]
+    print(output)
+
+def attention(V, Q, K):
+    output = [[None]*len(V[_]) for _ in range(len(V))]
+    tokens = len(V)
+    for i in range(tokens): 
+        wants = Q[i]
+        temp = []
+        temp1 = 0
+        for j in range(tokens):
+            #if j == i:
+                #continue #no need in skipping the current token ig
+            temp.append(sum([K[j][_] * wants[_] for _ in range(len(K[j]))])) #holy shit what are those nested brackets
+        temp1 = sum(temp) #for softmax
+        temp2 = [] #after softmax
+        for _ in range(len(temp)):
+            temp2.append(temp[_]/temp1)
+        output[i] = [
+            sum([temp2[j] * V[j][dim] for j in range(tokens)])
+            for dim in range(len(V[0]))
+        ]
+    print(output)
+
+
+VV = [[1,2,3],[4,5,6]]
+QQ = [[1,2,3],[4,5,6]]
+KK = [[1,2,3],[4,5,6]]
+
+tb.variables["0"] = {}
+tb.variables["0"]["layers"] = 4
+tb.variables["0"]["layer1"] = 50
+tb.variables["0"]["layer2"] = 200
+tb.variables["0"]["layer3"] = 200
+tb.variables["0"]["layer4"] = 20
+
+tb.initialize_nn(["0"])
+
+tb.nn_run(["0"])
+
+print(tb.variables["layer4n"])
